48e3c19c4e761e391c7cc67b08b7eb49cd0cbdbb
Danyel Lima Matos Granzotti <danyelgranzotti@alu.ufc.br>
2025-01-05 18:54:22 -0300

    feat: Add Dockerfile and docker-compose for application setup; implement data seeding for products

--- pharmacy_management_app/management/commands/seed.py
+++ pharmacy_management_app/management/commands/seed.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from pharmacy_management_app.models.bank_account import BankAccount
+from pharmacy_management_app.models.product import Product
 
 User = get_user_model()
 
@@ -11,6 +12,7 @@ class Command(BaseCommand):
         self.stdout.write('Seeding data...')
         self.create_users()
         self.create_bank_accounts()
+        self.create_products()
         self.stdout.write('Data seeded successfully.')
 
     def create_users(self):
@@ -40,3 +42,16 @@ class Command(BaseCommand):
                 self.stdout.write(f'Created bank account for user: {account_data["user"].email}')
             else:
                 self.stdout.write(f'Bank account already exists for user: {account_data["user"].email}')
+
+    def create_products(self):
+        products_data = [
+            {'name': 'Product 1', 'description': 'Description for product 1', 'cost_price': 5.00, 'profit_margin': 0.5, 'quantity': 100},
+            {'name': 'Product 2', 'description': 'Description for product 2', 'cost_price': 10.00, 'profit_margin': 0.6, 'quantity': 200},
+            {'name': 'Product 3', 'description': 'Description for product 3', 'cost_price': 15.00, 'profit_margin': 0.7, 'quantity': 300},
+        ]
+        for product_data in products_data:
+            product, created = Product.objects.get_or_create(name=product_data['name'], defaults=product_data)
+            if created:
+                self.stdout.write(f'Created product: {product.name}')
+            else:
+                self.stdout.write(f'Product already exists: {product.name}')
